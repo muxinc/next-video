41206d77b39150b9730d8b7c03facaa56c5957d3
Matthew McClure <matt@mux.com>
2023-08-03 12:37:28 -0700

    clean up import ordering

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-import NextVideo from './lib/NextVideo';
 import videoHandler from './lib/videoHandler';
-
 import localUploadHandler from './lib/handlers/localUpload';
-
+// Don't love this little race condition... we gotta figure that one out.
+// Basically we need to make sure all the handlers are registered before we start watching for files.
 videoHandler('local.video.added', localUploadHandler);
 
-// Don't love this little race condition... we gotta figure that one out.
+import NextVideo from './lib/NextVideo';
 import withNextVideo from './lib/withNextVideo';
 
 export { NextVideo, videoHandler, withNextVideo };
