8544db974817cadafa17e2f0effa507cafdfa4ae
Matthew McClure <matt@mux.com>
2023-08-07 15:12:21 -0700

    starting CLI work

--- src/cli.ts
+++ src/cli.ts
@@ -1,7 +1,21 @@
 #!/usr/bin/env node
 
-function main() {
-  console.log('Hello world!!');
-}
+import { Argv, Arguments } from 'yargs';
+import yargs from 'yargs/yargs';
 
-main();
+const init = {
+  command: 'init [dir]',
+  desc: 'Initializes next-video in a project.',
+  builder(yargs: Argv) {
+    return yargs.positional('dir', {
+      describe:
+        'The directory to use for your video-related files (including, but not limited to, the videos themselves',
+      default: 'video',
+    });
+  },
+  handler(argv: Arguments) {
+    console.log('init called for dir', argv.dir);
+  },
+};
+
+yargs(process.argv.slice(2)).command(init).demandCommand().help().argv;
