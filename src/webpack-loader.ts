f32ee9af78e0fd46b537afa7399c9def1129a9cb
Wesley Luyten <me@wesleyluyten.com>
2023-08-07 15:47:19 -0500

    Make video json watchable

--- src/webpack-loader.ts
+++ src/webpack-loader.ts
@@ -1,7 +1,12 @@
+import path from 'node:path';
 import { readFile } from 'node:fs/promises';
 
 export default async function loader(this: any, source: any) {
-  const vid = await readFile(this.resourcePath + '.json', 'utf-8');
+  var jsonPath = path.resolve(this.resourcePath + '.json');
+
+  this.addDependency(jsonPath);
+
+  const vid = await readFile(jsonPath, 'utf-8');
 
   return `export default ${vid}`;
 }
