6946752a7c292b732a18332e0d525ed6757b0e90
Matthew McClure <matt@mux.com>
2023-08-29 14:25:14 -0700

    try our hand at editing some next configs

---
+++ src/cli/lib/next-config.ts
@@ -0,0 +1,85 @@
+import { builders, loadFile, generateCode, writeFile } from 'magicast';
+
+import fs from 'node:fs/promises';
+import path from 'node:path';
+
+import { PACKAGE_NAME } from '../../constants.js';
+
+const COMMON_TEMPLATE = `
+
+// Everything below here added by the ${PACKAGE_NAME} CLI wizard.
+// You should probably clean this up.
+
+const originalConfig = module.exports;
+
+const withNextVideo = require('${path.join(PACKAGE_NAME, 'process')}');
+
+module.exports = withNextVideo(originalConfig);
+`;
+
+function extensionToType(filePath: string) {
+  if (filePath.endsWith('.mjs')) {
+    return 'module';
+  }
+
+  return 'commonjs';
+}
+
+export default async function updateNextConfigFile(parentDir: string = './') {
+  let type: 'module' | 'commonjs' = 'commonjs';
+  let configPath: string | undefined = undefined;
+  let configContents: string = '';
+
+  const pathsToCheck = ['next.config.js', 'next.config.mjs'];
+
+  for (let i = 0; i < pathsToCheck.length; i++) {
+    console.log('asdf');
+    const filePath = path.join(parentDir, pathsToCheck[i]);
+    let exists;
+    try {
+      exists = await fs.stat(filePath);
+    } catch (e) {
+      exists = false;
+    }
+
+    if (exists) {
+      type = extensionToType(pathsToCheck[i]);
+      configPath = filePath;
+      configContents = await fs.readFile(filePath, 'utf-8');
+
+      break;
+    }
+  }
+
+  if (!configPath) {
+    throw { error: 'not_found' };
+  }
+
+  if (configContents.includes(PACKAGE_NAME)) {
+    throw { error: 'already_added' };
+  }
+
+  if (type === 'commonjs') {
+    await fs.appendFile(configPath, COMMON_TEMPLATE);
+
+    return { type, configPath };
+  }
+
+  if (type === 'module') {
+    const mod = await loadFile(configPath);
+
+    mod.imports.$add({
+      from: PACKAGE_NAME,
+      imported: 'withNextVideo',
+      local: 'withNextVideo',
+    });
+
+    const expressionToWrap = generateCode(mod.exports.default.$ast).code;
+    mod.exports.default = builders.raw(`withNextVideo(${expressionToWrap})`);
+
+    // @ts-ignore
+    writeFile(mod, configPath);
+
+    return { type, configPath };
+  }
+}
---
+++ tests/with-next-video.test.ts
@@ -0,0 +1,50 @@
+import assert from 'node:assert';
+import { describe, it } from 'node:test';
+
+import withNextVideo from '../src/with-next-video.js';
+
+describe('withNextVideo', () => {
+  it('should handle nextConfig being a function', async () => {
+    const nextConfig = (phase, { defaultConfig }) => {
+      /**
+       * @type {import('next').NextConfig}
+       */
+      const nextConfig = {
+        ...defaultConfig,
+      };
+      return nextConfig;
+    };
+
+    const result = await withNextVideo(nextConfig);
+
+    const configResult = await result('phase', { defaultConfig: {} });
+
+    assert(typeof configResult.webpack === 'function');
+  });
+
+  it('should handle nextConfig being a promise', async () => {
+    const nextConfig = async (phase, { defaultConfig }) => {
+      /**
+       * @type {import('next').NextConfig}
+       */
+      const nextConfig = {
+        ...defaultConfig,
+      };
+      return nextConfig;
+    };
+
+    const result = await withNextVideo(nextConfig);
+
+    const configResult = await result('phase', { defaultConfig: {} });
+
+    assert(typeof configResult.webpack === 'function');
+  });
+
+  it('should handle nextConfig being an object', async () => {
+    const nextConfig = {};
+
+    const result = await withNextVideo(nextConfig);
+
+    assert(typeof result.webpack === 'function');
+  });
+});
