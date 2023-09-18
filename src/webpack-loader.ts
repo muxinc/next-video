47f45df9d8adf8b40e8b6169a1675dab4b7db7bb
Wesley Luyten <me@wesleyluyten.com>
2023-09-18 10:32:16 -0500

    fix: no json file, alert msg style (#62)
    fixes #54 #51

--- src/webpack-loader.ts
+++ src/webpack-loader.ts
@@ -6,7 +6,19 @@ export default async function loader(this: any, source: any) {
 
   this.addDependency(jsonPath);
 
-  const vid = await readFile(jsonPath, 'utf-8');
+  let vid;
+
+  try {
+    vid = await readFile(jsonPath, 'utf-8');
+  } catch {
+
+    const originalFilePath = path.relative(process.cwd(), this.resourcePath);
+
+    vid = JSON.stringify({
+      status: 'source',
+      originalFilePath
+    })
+  }
 
   return `export default ${vid}`;
 }
