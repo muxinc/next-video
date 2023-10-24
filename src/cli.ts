dd2bd038aa9a0c4386128e343ba9bf2d895e3273
Wesley Luyten <me@wesleyluyten.com>
2023-10-23 21:17:04 -0500

    fix: env issue importing remote video source (#80)

--- src/cli.ts
+++ src/cli.ts
@@ -1,12 +1,18 @@
 #!/usr/bin/env node
+import process from 'node:process';
+import path from 'node:path';
 import nextEnv from '@next/env';
 import log from './logger.js';
-
-nextEnv.loadEnvConfig(process.cwd(), undefined, log);
-
 import yargs from 'yargs/yargs';
 
 import * as init from './cli/init.js';
 import * as sync from './cli/sync.js';
 
+nextEnv.loadEnvConfig(process.cwd(), undefined, log);
+
 yargs(process.argv.slice(2)).command(init).command(sync).demandCommand().help().argv;
+
+// Import the app's next.config.js file so the env variables
+// __NEXT_VIDEO_OPTS set in with-next-video can be used.
+import(path.resolve('next.config.js'))
+  .catch(() => log.error('Failed to load next.config.js'));
