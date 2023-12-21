cbd9de5353b6635d62e59ced32b4f23050840960
Wesley Luyten <me@wesleyluyten.com>
2023-12-20 19:04:34 -0600

    test: add more tests with lazy import

--- tests/components/video.test.tsx
+++ tests/components/video.test.tsx
@@ -1,7 +1,9 @@
 import assert from 'node:assert';
 import { test } from 'node:test';
-import { create } from 'react-test-renderer';
+import { setTimeout } from 'node:timers/promises';
+import { create, act } from 'react-test-renderer';
 import React from 'react';
+import asset from '../factories/BBB-720p-1min.mp4.json' assert { type: "json" };
 import Video from '../../src/components/video.js';
 
 test('renders a video container', () => {
@@ -13,4 +15,20 @@ test('renders a video container', () => {
 test('prepends a class to the video container', () => {
   const wrapper = create(<Video className="foo" />);
   assert.equal(wrapper.toJSON().props.className, 'foo next-video-container');
+});
+
+test('renders mux-player without source', async () => {
+  await import('@mux/mux-player-react');
+  const wrapper = create(<Video />);
+  assert.equal(wrapper.toJSON().children[1].type, 'mux-player');
+});
+
+test('renders mux-player with imported source', async () => {
+  await import('@mux/mux-player-react');
+  const wrapper = create(<Video src={asset} />);
+  assert.equal(wrapper.toJSON().children[1].type, 'mux-player');
+  assert.equal(
+    wrapper.root.findByType('mux-player').parent.parent.props.playbackId,
+    'zNYmqdvJ61gt5uip02zPid01rYIPyyzVRVKQChgSgJlaY'
+  );
 });
