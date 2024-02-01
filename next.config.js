51dd0fdf7e5502cc2a11c24f83b41dbd5449d361
Wesley Luyten <me@wesleyluyten.com>
2024-02-01 08:00:01 -0600

    fix: try next-video/process import

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 // This file is used in the tests to mock the next.config.js file!!!
 // It's not actually used by the library, it's just a mock file for testing purposes.
 
-import { withNextVideo } from './dist/cjs/process.js';
+import { withNextVideo } from 'next-video/process';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
