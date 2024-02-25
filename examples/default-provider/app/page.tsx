69faa7fbaa7860676b2f1fb3b18cde8d37323a93
Wesley Luyten <me@wesleyluyten.com>
2024-02-25 09:03:38 -0600

    examples: cleanup, responsive, fixes

--- examples/default-provider/app/page.tsx
+++ examples/default-provider/app/page.tsx
@@ -1,7 +1,12 @@
+import { Metadata } from 'next'
 import Video from 'next-video';
 import getStarted from '/videos/get-started.mp4';
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'next-video - Basic example',
+}
+
+export default function Page() {
   return (
     <>
       <main>
--- examples/default-provider/app/slotted-poster/page.tsx
+++ examples/default-provider/app/slotted-poster/page.tsx
@@ -1,9 +1,14 @@
+import { Metadata } from 'next';
 import Image from 'next/image';
 import Video from 'next-video';
 import getStarted from '/videos/get-started.mp4';
 import getStartedPoster from '/images/get-started-poster.jpg';
 
-export default function Home() {
+export const metadata: Metadata = {
+  title: 'next-video - Slotted poster image',
+}
+
+export default function Page() {
   return (
     <>
       <main>
@@ -11,9 +16,11 @@ export default function Home() {
           <Image
             slot="poster"
             src={getStartedPoster}
+            placeholder="blur"
             alt="Get started with Next Video"
             fill={true}
-            objectFit="cover"
+            sizes="50vw"
+            priority
           />
         </Video>
       </main>
