b4ea7a7ab3d33aa40dd218036e782374ba121ca6
Lars Bättig <me@l4rs.net>
2024-07-26 22:28:53 +0200

    feat: add Cloudflare R2 provider

--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -2,3 +2,4 @@ export * as mux from './mux/provider.js';
 export * as vercelBlob from './vercel-blob/provider.js';
 export * as backblaze from './backblaze/provider.js';
 export * as amazonS3 from './amazon-s3/provider.js';
+export * as cloudflareR2 from './cloudflare-r2/provider.js'
